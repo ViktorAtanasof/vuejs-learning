4ab331d0ac4d72e8c8a566fe9f8b25fbecbd816e
ViktorAtanasof <atanasowviktor@gmail.com>
2024-07-06 18:33:53 +0300

    Setup and run the Vue.js app

---
+++ Intro-to-Vue-3/main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quis.'
+        }
+    }
+})
