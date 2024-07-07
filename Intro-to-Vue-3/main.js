9737f49ec29463d3fd14d8278420527c00303a1c
ViktorAtanasof <atanasowviktor@gmail.com>
2024-07-07 14:17:13 +0300

    Learn and practice event handling in Vue.js

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quis.',
             image: './assets/images/socks_green.jpg',
@@ -8,9 +9,17 @@ const app = Vue.createApp({
             inventory: 10,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 })
