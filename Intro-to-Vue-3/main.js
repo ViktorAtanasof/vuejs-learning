3250097c031a84fc44204d98ca24a9164a8d38a0
ViktorAtanasof <atanasowviktor@gmail.com>
2024-07-07 10:10:22 +0300

    Learn and practice list rendering in Vue.js

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -6,6 +6,11 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
             inventory: 10,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ]
         }
     }
 })
