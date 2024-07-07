ac37c4ad4c92a56cde88bb85cdc5f907f577b085
ViktorAtanasof <atanasowviktor@gmail.com>
2024-07-07 14:58:23 +0300

    Learn and practice computed properties in Vue.js

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -3,23 +3,34 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quis.',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             url: 'https://www.vuemastery.com/',
-            inventory: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.cart += 1;
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inventory() {
+            return this.variants[this.selectedVariant].quantity;
         }
     }
 })
